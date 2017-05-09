e89b27cd271d74c15d6918d92967f526a8ea8f55
stambi <stefan@somarecords.ch>
2017-05-09 21:47:58 +0200

    commit

--- ripoff_office.sh
+++ ripoff_office.sh
@@ -47,20 +47,13 @@ echo "###### Forget Package Receipts ######" >> $logfile
 for pkg in "Excel" "OneNote" "PowerPoint" "Word"
 do
   pkgname=$(pkgutil --pkgs | grep $pkg)
+
   if [ -n "$pkgname" ]; then
     pkgutil --forget com.microsoft.package.Microsoft_$pkg
     echo "$(date): Forget Package Receipts: com.microsoft.package.Microsoft_$pkg" >> $logfile
   fi
 done
 
-<<<<<<< HEAD
-
-#Remove items from dock
-
-for dockitem in "Microsoft%20Word.app" "Microsoft%20OneNote.app" "Microsoft%20PowerPoint.app" "Microsoft%20Excel.app"
-do
-dloc=$(defaults read "/Users/$loggedinuser/Library/Preferences/com.apple.dock.plist" persistent-apps | grep _CFURLString\" | awk -v x="$dockitem" '$0~x {print NR-1}')
-=======
 #Remove item from dock
 echo "###### Remove Items from Dock ######" >> $logfile
 dloc=$(defaults read "/Users/$loggedinuser/Library/Preferences/com.apple.dock.plist" persistent-apps | grep _CFURLString\" | awk '/Microsoft%20Word.app/ {print NR-1}')
@@ -70,18 +63,11 @@ if [ -n "$dloc" ]; then
 fi
 
 dloc=$(defaults read "/Users/$loggedinuser/Library/Preferences/com.apple.dock.plist" persistent-apps | grep _CFURLString\" | awk '/Microsoft%20OneNote.app/ {print NR-1}')
->>>>>>> origin/master
 if [ -n "$dloc" ]; then
   /usr/libexec/PlistBuddy -c "Delete persistent-apps:$dloc" ~/Library/Preferences/com.apple.dock.plist
   echo "$(date): $dloc removed from dock" >> $logfile
 fi
-done
 
-
-
-<<<<<<< HEAD
-
-=======
 dloc=$(defaults read "/Users/$loggedinuser/Library/Preferences/com.apple.dock.plist" persistent-apps | grep _CFURLString\" | awk '/Microsoft%20PowerPoint.app/ {print NR-1}')
 if [ -n "$dloc" ]; then
   /usr/libexec/PlistBuddy -c "Delete persistent-apps:$dloc" ~/Library/Preferences/com.apple.dock.plist
@@ -93,7 +79,6 @@ if [ -n "$dloc" ]; then
   /usr/libexec/PlistBuddy -c "Delete persistent-apps:$dloc" ~/Library/Preferences/com.apple.dock.plist
   echo "$(date): $dloc removed from dock" >> $logfile
 fi
->>>>>>> origin/master
 
 killall Dock
 
